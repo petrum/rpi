eb1a4387a01636fd21c34c87997a3c54b7ef77af
Petru Marginean <petru.marginean@gmail.com>
2016-03-06 17:10:49 -0500

    misc

---
+++ ding/ding.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+import time
+import pigpio
+import datetime
+import os
+from time import sleep
+
+pi = pigpio.pi()
+data = 17
+
+## Raspberry Pi 2 Model B (J8 header)
+# 3.3 VDC o    o 2 5.0 VDC (Power) <======
+#         o    o 4 5.0 VDC
+#         o    o 6 Ground
+#         o    o 8
+# GND     o    o 10
+# GEN0 17 o    o 12
+#         o    o 14
+#         o    o 16 GPIO 23 
+#         o    o 18 GPIO 24 
+#         o    o
+
+def micro_sleep(microsec):
+    sleep(microsec / 1000000.)
+
+def ding(bits):
+    #print 'There are', len(bits), "bits"	
+    for i, b in enumerate(bits):
+        pi.write(data, 0)
+        if b == '0':
+            micro_sleep(700)
+            pi.write(data, 1)
+            micro_sleep(300)
+        else:
+            micro_sleep(400)
+            pi.write(data, 1)
+            micro_sleep(600)
+        pi.write(data, 0)
+        #print i, b
+        
+for i in range(10):
+    micro_sleep(1000 * 20)
+    ding("0000001110111")
