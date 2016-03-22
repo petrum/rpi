b261df5939705e41a7b2f315254e60d0eba71b8c
Petru Marginean <petru.marginean@gmail.com>
2016-03-22 19:28:46 -0400

    misc

--- ding/ding.py
+++ ding/ding.py
@@ -1,36 +1,23 @@
 #!/usr/bin/python
 
 import time
-import pigpio
-import datetime
-import os
-from time import sleep
-import sys
-
-pi = pigpio.pi()
-data = 17
+import RPi.GPIO as GPIO
 
 ## Raspberry Pi 2 Model B (J8 header)
-# 3.3 VDC o    o 2 5.0 VDC (Power) <======
-#         o    o 4 5.0 VDC
-#         o    o 6 Ground
-#         o    o 8
-# GND     o    o 10
-# GEN0 17 o    o 12
-#         o    o 14
-#         o    o 16 GPIO 23 
-#         o    o 18 GPIO 24 
-#         o    o
+# 3.3 VDC 1  o    o 2 5.0 VDC (Power) <======
+#         3  o    o 4 5.0 VDC
+#         5  o    o 6 Ground
+#         7  o    o 8
+# GND     9  o    o 10
+# GEN017 11  o    o 12
+#            o    o 14
+#            o    o 16 GPIO 23 
+#            o    o 18 GPIO 24 
+#            o    o
 
-s = time.time() * 1000000
-pi.write(data, 0)
-print time.time() * 1000000 - s
-
-s = time.time() * 1000000
-pi.write(data, 1)
-print time.time() * 1000000 - s
-
-sys.exit(0)
+data = 11
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(data, GPIO.OUT)
 
 def micro_sleep(microsec):
     m = 1000000
@@ -43,16 +30,15 @@ def micro_sleep(microsec):
             return
 
 def set(d, b):
-    micro_sleep(400)
     if b == '0':
-        micro_sleep(300)
-        pi.write(d, 1)
+        micro_sleep(700)
+        GPIO.output(d, True)
         micro_sleep(300)
     else:
-        pi.write(d, 1)
+        micro_sleep(400)
+        GPIO.output(d, False)
         micro_sleep(600)
-    pi.write(d, 0)
-     
+    GPIO.output(d, False)
 
 def ding(bits):
     #print 'There are', len(bits), "bits"	
