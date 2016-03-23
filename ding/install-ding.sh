0372c408105180432a0d7064c167472728cac3df
Petru Marginean <petru.marginean@gmail.com>
2016-03-22 20:20:41 -0400

    misc

--- ding/install-ding.sh
+++ ding/install-ding.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 . "$(dirname $0)/../install-reuse.sh"
 READER=$(selectMicroSD)
-#diskcopy /home/petrum/Downloads/2015-11-21-raspbian-jessie.img $READER
-#diskcopy /home/petrum/Downloads/2016-02-26-raspbian-jessie-lite.img $READER
 diskcopy /home/petrum/Downloads/2016-03-18-raspbian-jessie-lite.img $READER
 expandFS $READER
 DEST=$(mountFS $READER)
-aptget $DEST
 networkSetup ~/github/rpi/net $DEST
 sethostname dinger $DEST
-pigpioInstall $DEST/home/pi/pigpio $DEST/home/pi/setup.sh
 get-rpi ~/github/rpi $DEST
-cat <<EOF >> $DEST/home/pi/setup.sh
-sudo reboot
-EOF
-cat $DEST/home/pi/setup.sh
 umountFS $DEST
 
