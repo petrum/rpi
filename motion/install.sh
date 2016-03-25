ab465908d239fbe5183df3d45e1516c163b0e18c
Petru Marginean <petru.marginean@gmail.com>
2016-03-25 09:51:28 -0400

    misc

--- motion/install.sh
+++ motion/install.sh
@@ -7,7 +7,7 @@ expandFS $READER
 DEST=$(mountFS $READER 2)
 sethostname motion $DEST
 get_rpi $DEST
-sudo sed -i 's|^exit 0|/home/pi/git/rpi/motion/motion.py > /tmp/motion.log 2>\&1\nexit 0|g' $DEST/etc/rc.local
+sudo sed -i 's|^exit 0|/home/pi/git/rpi/motion/motion.py >> /tmp/motion.log 2>\&1\nexit 0|g' $DEST/etc/rc.local
 sudo mkdir -p $DEST/root/.ssh
 sudo cp -v /home/petrum/rpi-private/id_rsa $DEST/root/.ssh
 sudo chmod 600 $DEST/root/.ssh/id_rsa
