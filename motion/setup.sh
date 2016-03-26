0c702ac6ce0c45b2a4475e415a162f3079089940
Petru Marginean <petru.marginean@gmail.com>
2016-03-26 12:32:46 -0400

    misc

--- motion/setup.sh
+++ motion/setup.sh
@@ -2,13 +2,26 @@
 
 ip=$(hostname -I) || true
 if [ "$ip" ]; then
+    echo "No IP..."
     exit 1
 fi
 if grep --quiet AuthUser /etc/ssmtp/ssmtp.conf; then
+    echo "Everything looks fine already..."
     exit 0
 fi
-apt-get update --fix-missing || exit 2
-apt-get install ssmtp mailutils vim -y || exit 3
-cat /home/pi/ssmtp.conf | tee -a /etc/ssmtp/ssmtp.conf || exit 4
+if ! apt-get update --fix-missing >/dev/null 2>&1 ; then
+   echo 'Failed to update...'
+   exit 2
+fi
+   
+if ! apt-get install ssmtp mailutils vim -y >/dev/null 2>&1 ; then
+    echo 'Failed to install...'
+    exit 3
+fi
+if ! cat /home/pi/ssmtp.conf >> /etc/ssmtp/ssmtp.conf ; then
+    echo 'Failed to add credentials...'
+    exit 4
+fi
+echo 'All setup'
 exit 0
 
