7196bcd41a6392fc3fb1cd75e72fe1c8089ab2d1
Petru Marginean <petru.marginean@gmail.com>
2016-03-26 22:23:06 -0400

    misc

--- weather/setup.sh
+++ weather/setup.sh
@@ -10,21 +10,24 @@ if [[ -f /root/setup.done ]] ; then
     echo "Everything looks fine already..."
     exit 0
 fi
-
+echo "Updating..."
 if ! apt-get update --fix-missing >/dev/null 2>&1 ; then
    echo 'Failed to update...'
    exit 2
 fi
-   
+echo "done"
+echo "Installing..."
 #if ! apt-get install python-pip python-dev vim -y >/dev/null 2>&1 ; then
 #    echo 'Failed to install...'
 #    exit 3
 #fi
-
+echo "done"
+echo "pip installing spidev..."
 #if ! pip install spidev >/dev/null 2>&1 ; then
 #    echo 'Failed to install spidev...'
 #    exit 3
 #fi
+echo "done"
 
 touch /root/setup.done
 echo 'All setup'
