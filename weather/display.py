03e1da3adcb3c4f8bbacd6d15fe0df8554b2aa52
Petru Marginean <petru.marginean@gmail.com>
2016-03-26 21:23:34 -0400

    misc

--- weather/display.py
+++ weather/display.py
@@ -3,6 +3,7 @@ import MAX7219array as m7219
 from MAX7219fonts import CP437_FONT, SINCLAIRS_FONT, LCD_FONT, TINY_FONT
 from MAX7219array import DIR_L, DIR_R, DIR_U, DIR_D
 import sys
+import time
 
 f = sys.argv[1]
 
