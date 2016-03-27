1db328546ecdae4af18c972fef6b092a2265c188
Petru Marginean <petru.marginean@gmail.com>
2016-03-26 20:07:51 -0400

    misc

--- weather/weather.sh
+++ weather/weather.sh
@@ -4,5 +4,6 @@ if [[ ! -e '/tmp/weather.txt' ]]; then
     echo 'Nothing yet' > '/tmp/weather.txt'
 fi
 
-PYTHONPATH=/home/pi/MAX7219array "$(dirname $0)"/weather.py
+crontab -u pi < crontab
+PYTHONPATH=/home/pi/MAX7219array "$(dirname $0)"/display.py /home/pi/weather.txt
 
