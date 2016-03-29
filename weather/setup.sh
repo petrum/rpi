510f546247c852c6814061c186229e278f29cc90
Your Name <you@example.com>
2016-03-29 13:42:16 -0400

    misc

--- weather/setup.sh
+++ weather/setup.sh
@@ -12,25 +12,24 @@ if [[ -f /root/setup.done ]] ; then
     /home/pi/git/rpi/weather/display.py Everything looks fine already
     exit 0
 fi
-/home/pi/git/rpi/weather/display.py Updating please wait
+/home/pi/git/rpi/weather/display.py Updating please wait...
 if ! apt-get update --fix-missing >> $LOG 2>&1 ; then
    /home/pi/git/rpi/weather/display.py Failed to update
    exit 2
 fi
 /home/pi/git/rpi/weather/display.py done
-/home/pi/git/rpi/weather/display.py Installing please wait
+/home/pi/git/rpi/weather/display.py Installing please wait...
 if ! apt-get install python-pip python-dev vim -y >> $LOG 2>&1 ; then
     /home/pi/git/rpi/weather/display.py Failed to install
     exit 3
 fi
 /home/pi/git/rpi/weather/display.py done
-/home/pi/git/rpi/weather/display.py pip installing spidev please wait
+/home/pi/git/rpi/weather/display.py pip installing spidev please wait...
 if ! pip install spidev >> $LOG 2>&1 ; then
     /home/pi/git/rpi/weather/display.py Failed to install spidev
     exit 3
 fi
 /home/pi/git/rpi/weather/display.py done all
 touch /root/setup.done
-/home/pi/git/rpi/weather/display.py All setup
 exit 0
 
