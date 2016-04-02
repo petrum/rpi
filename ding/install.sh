6773eb0ea1aa90a64fead4db5a758ba47a26ea9f
Petru Marginean <petru.marginean@gmail.com>
2016-04-02 16:49:52 -0400

    misc

--- ding/install.sh
+++ ding/install.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 . "$(dirname $0)/../install-reuse.sh"
 READER=$(selectMicroSD)
-#diskcopy /home/petrum/Downloads/2016-03-18-raspbian-jessie.img $READER
 diskcopy /home/petrum/Downloads/2016-03-18-raspbian-jessie-lite.img $READER
 expandFS $READER
 DEST=$(mountFS $READER 2)
 generic_setup $DEST
 static_ip '192.168.1.8' '192.168.1.1' $DEST
 sethostname dinger $DEST
-get_rpi $DEST
 umountFS $DEST
 
--- motion/install.sh
+++ motion/install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 . "$(dirname $0)/../install-reuse.sh"
 READER=$(selectMicroSD)
-#diskcopy /home/petrum/Downloads/2016-03-18-raspbian-jessie.img $READER
 diskcopy /home/petrum/Downloads/2016-03-18-raspbian-jessie-lite.img $READER
 expandFS $READER
 DEST=$(mountFS $READER 2)
@@ -11,3 +10,4 @@ dynamic_ip 192.168.1.1 255.255.255.0 $DEST
 sudo sed -i 's|^exit 0|/home/pi/git/rpi/motion/startup.sh >> /root/motion.log 2>\&1\nexit 0|g' $DEST/etc/rc.local
 sudo cp -v /home/petrum/rpi-private/ssmtp.conf $DEST/home/pi/ssmtp.conf
 umountFS $DEST
+
