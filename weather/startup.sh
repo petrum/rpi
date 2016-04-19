fc60e1e7d2fe1bb98e4525ebbaf5bf55bbe719a2
Petru Marginean <petru.marginean@gmail.com>
2016-04-19 12:22:55 -0400

    misc

--- weather/startup.sh
+++ weather/startup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd /home/pi/git/rpi/weather
-crontab -u pi crontab
 export PYTHONPATH=/home/pi/MAX7219array
 
 echo "Started at " $(date)
@@ -10,6 +9,7 @@ while ! /bin/bash ./setup.sh ; do
 done
 echo "Done at " $(date)
 sync
+crontab -u pi crontab
 while :
 do
     cat /home/pi/weather.txt | /home/pi/git/rpi/weather/display.py
