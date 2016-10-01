65175fb39a55333173e3ccc7e22924ea87f97d3c
Petru Marginean <petru.marginean@gmail.com>
2016-09-30 22:41:11 -0400

    misc

--- motion/install.sh
+++ motion/install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 . "$(dirname $0)/../install-reuse.sh"
 READER=$(selectMicroSD)
-diskcopy /home/petrum/Downloads/2016-03-18-raspbian-jessie-lite.img $READER
-expandFS $READER
+diskcopy /home/petrum/Downloads/2016-09-23-raspbian-jessie-lite.img $READER
 DEST=$(mountFS $READER 2)
 generic_setup $DEST $READER
 sethostname motion $DEST
