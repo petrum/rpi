6061471247b3efba86a9c1834bed4ada61afb7a1
Petru Marginean <petru.marginean@gmail.com>
2017-11-11 16:08:28 -0500

    misc

--- clock/clock.py
+++ clock/clock.py
@@ -5,18 +5,11 @@ import max7219.font as font
 import sys
 import time
 import fileinput
+import datetime
 
 device = led.matrix(cascaded=8)
 device.orientation(90)
 device.brightness(3)
 
-if len(sys.argv) == 1:
-  for line in sys.stdin:
-    #device.clear()
-    device.show_message(line.rstrip('\n'), delay=.004)
-  sys.exit(0)
-print sys.argv
-for word in sys.argv[1:]:
-  device.show_message(word, delay=1)
-  time.sleep(.5)
+print strftime("%Y-%m-%d %H:%M:%S", datetime.datetime.now())
   
