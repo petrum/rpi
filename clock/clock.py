2ae0160f4d469cabcd60253a676b77312fcd8b23
Petru Marginean <petru.marginean@gmail.com>
2017-11-11 18:52:55 -0500

    misc

--- clock/clock.py
+++ clock/clock.py
@@ -7,10 +7,22 @@ import time
 import fileinput
 import datetime
 
+sys.path.insert(0, '..')
+import connected
+
 device = led.matrix(cascaded=8)
 device.orientation(90)
 device.brightness(3)
 
+isConnected = False
+last = ''
 while True:
-    device.show_message(datetime.datetime.now().strftime('%H:%M:%S'), delay=0)
-
+    t = datetime.datetime.now().strftime('%H:%M:%S')
+    if t[-2:] == '00':
+        isConnected = connected.test()
+    t.replace(':', ';')
+    if last == t:
+        continue
+    #print t
+    device.show_message(t, delay=0)
+    last = t
--- connected.py
+++ connected.py
@@ -4,13 +4,10 @@ import socket
 import sys
 
 REMOTE_SERVER = "www.google.com"
-def is_connected():
+
+def test():
   try:
-    # see if we can resolve the host name -- tells us if there is
-    # a DNS listening
     host = socket.gethostbyname(REMOTE_SERVER)
-    # connect to the host -- tells us if the host is actually
-    # reachable
     s = socket.create_connection((host, 80), 2)
     return True
   except:
@@ -18,4 +15,4 @@ def is_connected():
   return False
 
 if __name__ == '__main__':
-    sys.exit(0 if is_connected() else -1)
+    sys.exit(0 if test() else -1)
