5270801b85440976c2d720469c76ed732e7ea3b5
Petru Marginean <petru.marginean@gmail.com>
2017-11-11 18:33:40 -0500

    misc

---
+++ connected.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import socket
+import sys
+
+REMOTE_SERVER = "www.google.com"
+def is_connected():
+  try:
+    # see if we can resolve the host name -- tells us if there is
+    # a DNS listening
+    host = socket.gethostbyname(REMOTE_SERVER)
+    # connect to the host -- tells us if the host is actually
+    # reachable
+    s = socket.create_connection((host, 80), 2)
+    return True
+  except:
+     pass
+  return False
+
+sys.exit(0 if is_connected() else -1)
+
