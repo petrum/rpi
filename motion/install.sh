3c429fcc2c4f3a62f93f87ae82bc4ce248e44641
Petru Marginean <petru.marginean@gmail.com>
2021-11-07 18:43:23 -0500

    isc

--- motion/install.sh
+++ motion/install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 . "$(dirname $0)/../install-reuse.sh"
 READER=$(selectMicroSD)
-diskcopy /home/petrum/raspbian.img/2016-09-23-raspbian-jessie-lite.img $READER
+diskcopy /home/petrum/raspbian.img/2017-09-07-raspbian-stretch-lite.img $READER
 DEST=$(mountFS $READER 2)
 generic_setup $DEST $READER
 sethostname motion $DEST
-dynamic_ip 192.168.1.1 255.255.255.0 $DEST
+dynamic_ip 10.0.0.1 255.255.255.0 $DEST
 sudo sed -i 's|^exit 0|/home/pi/git/rpi/motion/startup.sh petru.marginean@gmail.com >> /root/motion.log 2>\&1\nexit 0|g' $DEST/etc/rc.local
 sudo cp -v /home/petrum/rpi-private/ssmtp.conf $DEST/home/pi/ssmtp.conf
 umountFS $DEST
