68fc00cf619677ac4db5bddf7a788cf7191f33fe
Petru Marginean <petru.marginean@gmail.com>
2021-11-07 17:17:17 -0500

    isc

--- ding/install.sh
+++ ding/install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 . "$(dirname $0)/../install-reuse.sh"
 READER=$(selectMicroSD)
-diskcopy /home/petrum/raspbian.img/2016-09-23-raspbian-jessie-lite.img $READER
+diskcopy /home/petrum/raspbian.img/2017-09-07-raspbian-stretch-lite.img $READER
 DEST=$(mountFS $READER 2)
 generic_setup $DEST $READER
-static_ip '10.0.0.30' '10.0.0.1' $DEST
+#static_ip '10.0.0.30' '10.0.0.1' $DEST
+dynamic_ip 10.0.0.1 255.255.255.0 $DEST
 sethostname dinger $DEST
 umountFS $DEST
 
