e5834490d09c930076a1cb182b07f835b5629c2f
Petru Marginean <petru.marginean@gmail.com>
2021-11-07 10:58:28 -0500

    isc

--- ding/install.sh
+++ ding/install.sh
@@ -4,7 +4,7 @@ READER=$(selectMicroSD)
 diskcopy /home/petrum/raspbian.img/2016-09-23-raspbian-jessie-lite.img $READER
 DEST=$(mountFS $READER 2)
 generic_setup $DEST $READER
-static_ip '192.168.1.8' '192.168.1.1' $DEST
+static_ip '10.0.0.30' '10.0.0.1' $DEST
 sethostname dinger $DEST
 umountFS $DEST
 
